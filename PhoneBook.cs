32aa5787eee8b9eaaab93ca0b9afaf04aaab1f47
MRPhillip007 <filipplyamov@gmail.com>
2022-08-24 17:29:00 +0300

    phoneBook initial commit

---
+++ PhoneBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHw
+{
+    class PhoneBook
+    {
+        // TODO
+        // 1. fill the phonebook. Create| PhoneBook mainBook = new PhoneBook("main.txt");
+        // 2. Searching contact (name, surname, phone)| mainBook.SearchContact(name: "Алекс");
+        // 3. Add contact| mainBook.AddContact(name: "Peter", surname: "Peterson", phone:"380975673423");
+        // 4. View contacts| mainBook.ViewContacts();
+
+
+
+    }
+}
